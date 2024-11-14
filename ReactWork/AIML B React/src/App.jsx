f0e71bb81c1abef0b61604c7c5a58677ddce2135
Shubham Maheshwari <145970862+ShubhamMaheshwari-04@users.noreply.github.com>
2024-11-14 19:06:55 +0530

    React Work done successfully

---
+++ ReactWork/AIML B React/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  let a = 20;
+
+  let mystyle = {
+    backgroundColor: "cyan",
+    color: "red"
+  };
+  return (
+    <div style={{color:"red"}}>
+      <h2>ABES ENGINEERING COLLEGE</h2>
+    </div> 
+  )
+}
+
+export default App;
